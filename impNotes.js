0b9ef6b509b815e7c4bf7c31485c41a283a9edab
unknown <maheshbarapatre14@gmail.com>
2024-07-09 20:02:39 +0530

    added more ques

---
+++ impNotes.js
@@ -0,0 +1,22 @@
+var smallestNum = Number.MIN_SAFE_INTEGER;
+var largestNum = Number.MAX_SAFE_INTEGER;
+
+// for...in ///iterating in object
+// for...of ///iterating in array
+
+// Math.max(...arr); ///max num in arr
+var arr = [2, 4, 5];
+var obj = {
+  name: "amehs",
+};
+
+let num = 23;
+let s = "mahes";
+let flag = true;
+
+console.log(Array.isArray(arr));
+console.log(Array.isArray(obj));
+console.log(typeof obj);
+console.log(typeof s);
+console.log(typeof num);
+console.log(typeof flag);
